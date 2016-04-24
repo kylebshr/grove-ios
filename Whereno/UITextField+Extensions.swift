cf5a325d0e27a681d07e8a89614e0b7e4b3c0cbc
Kyle Bashour <kylebshr@me.com>
2016-04-24 19:37:29 -0400

    Add UITextField extension for future use

---
+++ Whereno/UITextField+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  UITextField+Extensions.swift
+//  Whereno
+//
+//  Created by Kyle Bashour on 4/24/16.
+//  Copyright © 2016 Kyle Bashour. All rights reserved.
+//
+
+import UIKit
+
+private var kAssociationKeyNextField: UInt8 = 0
+
+extension UITextField {
+
+    @IBOutlet var nextField: UITextField? {
+        get {
+            return objc_getAssociatedObject(self, &kAssociationKeyNextField) as? UITextField
+        }
+        set(newField) {
+            objc_setAssociatedObject(self, &kAssociationKeyNextField, newField, .OBJC_ASSOCIATION_RETAIN)
+        }
+    }
+}
